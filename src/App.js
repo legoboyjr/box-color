1284e7f2d63bb81857b3e4e8b4b7a32a7c4f9ae2
Alexander Thompson <legoboyjr@gmail.com>
2018-11-02 20:55:05 -0400

    started stuff

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        it works
+        <header className="app-header">
+        <h1>Color-Box</h1>
+        </header>
+        <main className="app-main">
+          <div className="box">
+
+          </div>
+          <div className="color">
+
+          </div>
+        </main>
+        <footer className="app-footer">
+
+        </footer>
       </div>
     );
   }
