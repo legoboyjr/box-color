9ff31d40e52cd7dee8df1a4e05b07b0639c630c7
Alexander Thompson <legoboyjr@gmail.com>
2018-11-05 18:39:55 -0500

    started stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
+import { HuePicker } from 'react-color';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      boxColor: 'white',
+      selectedColor: '#ffff00'
+    };
+    this.onColorSelect = this.onColorSelect.bind(this);
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+  onColorSelect(color, event) {
+    this.setState({
+      selectedColor: color.hex
+    });
+  }
+
+  changeBackground(event) {
+    this.setState({
+      boxColor: this.state.selectedColor
+    });
+  }
   render() {
+    const boxStyles = {
+      backgroundColor: this.state.boxColor
+    }
     return (
-      <div className="App">
-        <header className="app-header">
-        <h1>Color-Box</h1>
+      <div className="App-container">
+        <header className="App-header">
+          <h1 className="title">Color Box</h1>
         </header>
-        <main className="app-main">
-          <div className="box">
-
-          </div>
-          <div className="color">
-
-          </div>
+        <main className="App-main">
+          <div className="box" onClick={this.changeBackground} style={boxStyles}></div>
+          <HuePicker onChange={this.onColorSelect} color={this.state.selectedColor} /> 
         </main>
-        <footer className="app-footer">
-
+        <footer className="App-footer">
+          <p>Made by me!</p>
         </footer>
       </div>
     );
